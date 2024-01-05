8a6b418e3397541653489865279bcca6ee8c9ca4
abdulmelik <abdulmelik@gmail.com>
2024-01-05 22:29:19 +0300

    Add project files.

---
+++ DataAccess/Concrete/EntityFramework/SimpleContextDb.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework;
+public class SimpleContextDb:DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("server=.\\SQLEXPRESS;Database=DemoDb;Integrated Security=true;");
+    }
+
+    public DbSet<User>? Users { get; set; }
+    public DbSet<OperationClaim>? OperationClaims { get; set; }
+    public DbSet<UserOperationClaim>? UserOperationClaims { get; set; }
+}
